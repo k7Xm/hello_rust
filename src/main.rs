7aa031ff4409da98c91911422f4a647b845fe8ae
k7Xm <zn81x6@gmail.com>
2026-05-05 09:54:03 +0800

    review: borrowing rules practice

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-fn create_string() -> String{
-    let s = String::from("hello");
-    s
-}
-
 fn main(){
-    let result = create_string();
-    println!("{}", result);
+    let mut s = String::from("hello");
+    let r1 = & s;
+    let r2 = &mut s;
+    println!("{}, {}", r1, r2);
 }
