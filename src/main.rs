b5c806f430139c4d0dfacdb21c83d5b50521efd7
k7Xm <zn81x6@gmail.com>
2026-05-06 11:32:55 +0800

    #07 enum + match + Option

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-struct PP (f64,f64,);
-fn main() {
-    let p = PP(1.0, 2.0);
+fn find_max(nub: &[i32]) -> Option<i32> {
+    let mut i: usize = 0;
 
-    println!("first coordinate is \"{}\"; Second coordinate is \"{}\".", p.0, p.1);
+    if nub.get(0) == None {
+        return None
+    }
+    let mut m = nub[0];
+    while nub.get(i) != None {
+        if nub[i] >= m {
+            m = nub[i];
+        } 
+        i += 1;
+    }
+    return Some(m)
+}
+
+fn main() {
+    let nums1: &[i32] = &[3, 9 ,7, 2, 4];
+    let nums_empty: &[i32] = &[];
+
+    match find_max(&nums1) {
+    Some(n) => println!("The max number is {}", n),
+    None => println!("Empty Input."),
+    }
+
+    match find_max(&nums_empty) {
+    Some(n) => println!("The max number is {}", n),
+    None => println!("Empty Input."),
+    }
 }
