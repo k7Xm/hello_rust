e76aa737979915430af628efebd776990c8187df
k7Xm <zn81x6@gmail.com>
2026-05-07 14:11:48 +0800

    #09 trait + bound

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,15 @@
-trait Jieshao {
-    fn jieshao(&self) -> String;
-}
+use std::fmt;
 
-struct Maozi {
-    yanse: String,
-}
-
-struct Xiezi {
-    chicun: i32,
-}
-
-impl Jieshao for Maozi {
-    fn jieshao(&self) -> String {
-        format!("一顶{}的帽子。", self.yanse)
-    }
-}
-
-impl Jieshao for Xiezi {
-    fn jieshao(&self) -> String {
-        format!("一双{}码的鞋子。", self.chicun)
-    }
-}
-
-fn show(item: &impl Jieshao) {
-    println!("{}", item.jieshao());
+fn describe<T>(val: &T) 
+where
+    T: fmt::Debug + fmt::Display,
+{
+    println!("Debug: {:?}", val);
+    println!("Display: {}", val);
 }
 
 fn main() {
-    let redhat = Maozi {
-        yanse: String::from("红色"),
-    };
-
-    let nikeshoes = Xiezi { chicun: 42 };
-
-    show(&redhat);
-    show(&nikeshoes);
+    describe(&42);
+    describe(&"hello");
+    describe(&String::from("world"));
 }
