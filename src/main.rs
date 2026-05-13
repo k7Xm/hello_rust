4251f94b089a7755a4c5c1c58a56659bb21763ad
k7Xm <zn81x6@gmail.com>
2026-05-13 19:07:30 +0800

    #14 2026-5-13

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-mod math;
-
-use math::{add, divide, multiply, subtract};
-
-fn main() {
-    println!("{}", add(7.0, 3.0));
-    println!("{}", multiply(7.0, 4.0));
-    println!("{}", subtract(7.0, 0.0));
-
-    match divide(10.0, 2.0) {
-        Ok(v) => println!("{}", v),
-        Err(e) => println!("Error, {}", e),
-    }
-
-    match divide(10.0, 0.0) {
-        Ok(v) => println!("{}", v),
-        Err(e) => println!("Error, {}", e),
-    }
+fn apply_all(inputs: &[i32], ops: &[Box<dyn Fn(i32) -> i32>]) -> Vec<i32> {
+    inputs
+        .iter()
+        .map(|&x| ops.iter().fold(x, |acc, op| op(acc)))
+        .collect()
 }
